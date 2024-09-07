2a1eeec7b79e09293a2fd13ae9da0169a298ae7d
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2024-09-07 02:35:42 +0000

    continue

--- src/app/(routes)/dashboard/_components/SideNav.jsx
+++ src/app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import Image from "next/image";
 
-import {LayoutGrid, PiggyBank, ReceiptText, ShieldCheck, CircleDollarSign} from 'lucide-react'
+import {LayoutGrid, PiggyBank, ReceiptText, ShieldCheck, CircleDollarSign, TrendingUp, TrendingDownIcon} from 'lucide-react'
 
 import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
@@ -13,6 +13,49 @@ function SideNav() {
             id: 1,
             name: "Dashboard",
             icon: LayoutGrid,
+            path: "/dashboard",
+        }
+        {
+            id: 2,
+            name: "Incomes",
+            icon: CircleDollarSign,
+            path: "/dashboard/incomes",
+        }
+        {
+            id: 3,
+            name: "Expenses",
+            icon: ReceiptText,
+            path: "/dashboard/expenses",
+        }
+        {
+            id: 4,
+            name: "Upgrade",
+            icon: ShieldCheck,
+            path: "/dashboard/upgrade",
         }
     ]
+
+    const path = userPathname();
+
+    useEffect(() => {
+        console.log(path)
+    }, [path]);
+
+    return(
+        <div className='h-screen p=5 border shadow-sm'>
+            <div className="flex flex-row items-center">
+               <Image src={'./'} alt='logo' width={40} height={25}/>
+               <span className="text-green-600 font-bold text-xl">PiggySmart</span>
+            </div>
+            <div className="mt-5">
+                {menuList.map((menu, index)=>(
+                    <Link href={menu.path} key={index}>
+                        <h2 className={`flex gap-2 items-center text-gray-500 font-medium mb-2 p-4 cursor-pointer rounded-full hover:text-primary hover:bg-green-100 ${path === menu.path && "text-primary bg-green-100"}`}>
+                            <menu.icon/>
+                        </h2>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
